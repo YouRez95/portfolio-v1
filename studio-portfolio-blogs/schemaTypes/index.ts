2e90acd0aadb0c05c2dd3b554efc704d72129c25
Youness Elalouani <younesselalouani1995@gmail.com>
2025-02-17 10:05:43 +0100

    Removed folder from Git tracking

--- studio-portfolio-blogs/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import blog from './blog'
-import feedback from './feedback'
-import project from './project'
-import projectDetail from './projectDetail'
-
-export const schemaTypes = [blog, project, projectDetail, feedback]
