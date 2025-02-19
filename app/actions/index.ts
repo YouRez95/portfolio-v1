72c3d0d8ab0f72c103ed1d135e4d6d902c56fe62
Youness Elalouani <younesselalouani1995@gmail.com>
2025-02-19 13:03:46 +0100

    set up the email

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -24,8 +24,8 @@ export const sendEmail = async ({
   const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: process.env.RECEIVER_EMAIL as string,
+      from: process.env.SENDER_EMAIL as string,
+      to: "",
       subject: `New message from portfolio`,
       html: `
       <p><strong>Name:</strong> ${name}</p>
