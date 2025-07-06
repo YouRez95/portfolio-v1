280051848e159b3a7a569a079fc654e188271143
Youness Elalouani <younesselalouani1995@gmail.com>
2025-07-06 15:53:17 +0100

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { prozaLibre } from "@/fonts/fonts";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 export const metadata: Metadata = {
   title: "Youness Elalouani",
@@ -14,6 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics />
       <body className={`${prozaLibre.className} overflow-x-hidden bg-primary`}>
         {children}
       </body>
